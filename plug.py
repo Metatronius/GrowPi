fcb8cb32ab2c38e128dcbb2ab89b0952aa1d6c70
EWrigh <wrightelliot106@gmail.com>
2025-05-27 15:23:30 -0500

    finished plug.py

---
+++ plug.py
@@ -0,0 +1,29 @@
+import asyncio
+from kasa import Discover
+
+async def findDiviceIp(usern,pas):
+    dev = await Discover.discover(username=str(usern),password=str(pas))
+    lis=[]
+    for devices in dev.values():
+        await devices.update()
+        lis.append(devices.host)
+    return lis
+
+async def turnOn(ip,usern,pas):
+    dev = await Discover.discover_single(str(ip),username=str(usern),password=str(pas))
+    await dev.turn_on()
+    await dev.update()
+
+async def turnOff(ip,usern,pas):
+    dev = await Discover.discover_single(str(ip),username=str(usern),password=str(pas))
+    await dev.turn_off()
+    await dev.update()
+
+async def turnToggle(ip,usern,pas):
+    dev= await Discover.discover_single(str(ip),username=str(usern),password=str(pas))
+    if dev.is_on():
+        await dev.turn_off()
+    else:
+        await dev.turn_on()
+    await dev.update
+
